a169ac1329bcb7f816d272294b8672d9021a7eba
hieple <hieplevuc@gmail.com>
2024-04-27 13:29:41 +0700

    feat: Exprort default r2 worker function

--- src/worker-r2/index.ts
+++ src/worker-r2/index.ts
@@ -1,16 +1,20 @@
 import { IEnv, IUploadR2, Method } from '../interfaces';
-export const r2Interact = async (method: Method, { file, fileName }: IUploadR2, env: IEnv) => {
-    switch (method) {
-      case 'PUT':
-        return env.FREEDOME_BUCKET.put(fileName, file);
-      case 'GET':
-        const object = await env.FREEDOME_BUCKET.get(fileName);
-        if (object === null) return null;
-        const headers = new Headers();
-        object.writeHttpMetadata(headers);
-        headers.set('etag', object.httpEtag);
-        return { ...object.body, ...headers };
-      case 'DELETE':
-        return env.FREEDOME_BUCKET.delete(fileName);
-    }
+export default async (
+  method: Method,
+  { file, fileName }: IUploadR2,
+  env: IEnv
+) => {
+  switch (method) {
+    case 'PUT':
+      return env.FREEDOME_BUCKET.put(fileName, file);
+    case 'GET':
+      const object = await env.FREEDOME_BUCKET.get(fileName);
+      if (object === null) return null;
+      const headers = new Headers();
+      object.writeHttpMetadata(headers);
+      headers.set('etag', object.httpEtag);
+      return { ...object.body, ...headers };
+    case 'DELETE':
+      return env.FREEDOME_BUCKET.delete(fileName);
   }
+};
